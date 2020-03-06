b217fa51b01e1c9be71eecb0a3502f0bcfb3fe4d
JuanF2019 <juan.martinez27@correo.icesi.edu.co>
2020-03-05 21:18:07 -0500

    Added missing Department.java file

---
+++ src/model/Department.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Department {
+	private Office firstOffice;
+	
+	public Department() {
+		firstOffice = null;
+	}
+	
+	public void addOffice(String c, int f) {
+		//Missing body...
+	}
+	
+	public Office searchOffice(String c) {
+		Office of = firstOffice;
+		//Incomplete method...
+		return of;
+	}
+	
+	public Office removeOffice(String c) {
+		Office of = firstOffice;
+		//Incomplete method...
+		return of;
+	}
+	
+	public void sortByCode() {
+		//Missing body...
+	}
+	
+	public void reomveEvenFloors() {
+		//Missing body...
+	}
+	
+}
