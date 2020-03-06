d11c8b1d313e814a5ea5c58603f20a030d16734b
JuanF2019 <juan.martinez27@correo.icesi.edu.co>
2020-03-06 11:50:30 -0500

    Finish addOffice() method of class Department.

--- src/model/Department.java
+++ src/model/Department.java
@@ -7,8 +7,18 @@ public class Department {
 		firstOffice = null;
 	}
 	
+	//Adds office at the end of the linked list.
 	public void addOffice(String c, int f) {
-		//Missing body...
+		Office of = firstOffice;
+		if(of == null) {
+			firstOffice = new Office(c,f,null);
+		}
+		else {
+			while(of.getNextOffice() != null) {
+				of = of.getNextOffice();
+			}
+			of.setNextOffice(new Office(c,f,null));
+		}
 	}
 	
 	public Office searchOffice(String c) {
