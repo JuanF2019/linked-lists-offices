72772439835a5ccddc176816083dd3e4fe60ce10
JuanF2019 <juan.martinez27@correo.icesi.edu.co>
2020-03-06 22:16:24 -0500

    Finish methods searchOffice and removeOffice from class Department.

--- src/model/Department.java
+++ src/model/Department.java
@@ -21,15 +21,39 @@ public class Department {
 		}
 	}
 	
+	//Searches an office given its code.
 	public Office searchOffice(String c) {
-		Office of = firstOffice;
-		//Incomplete method...
+		Office of = firstOffice;		
+		if(of != null && !of.getCode().equals(c)) {	
+			while(of.getNextOffice() != null && !of.getCode().equals(c)) {
+				of = of.getNextOffice();			
+			}		
+		}				
 		return of;
 	}
 	
+	//Removes an office given its code.
 	public Office removeOffice(String c) {
+		Office prevOf = null;
 		Office of = firstOffice;
-		//Incomplete method...
+		
+		if(of != null && !of.getCode().equals(c)) {				
+			while(of.getNextOffice() != null && !of.getCode().equals(c)) {
+				prevOf = of;
+				of = of.getNextOffice();			
+			}
+			if(of.getCode().equals(c)) {
+				if(of.getNextOffice() == null) {
+					prevOf.setNextOffice(null);
+				}
+				else {
+					prevOf.setNextOffice(of.getNextOffice());
+				}
+			}
+		}
+		else if(of != null && of.getCode().equals(c)) {			
+			firstOffice = of.getNextOffice();
+		}		
 		return of;
 	}
 	
